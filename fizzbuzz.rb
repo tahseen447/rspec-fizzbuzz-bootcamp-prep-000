b2f961d911ac7fad0dc9e39c5b8c7bfd55d8715e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-11 19:19:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(number)
-  a = 
+  a =
   b = number % 5
   puts "#{a}"
   puts "#{b}"
-  if(number % 3 && number % 5)
+  if(number % 3==0 && number % 5==0)
     "FizzBuzz"
-  elseif (number % 3)
+  elseif (number % 3==0)
   "Fizz"
-elseif (number % 50)
+elseif (number % 5==0)
 "Buzz"
 else
   nil

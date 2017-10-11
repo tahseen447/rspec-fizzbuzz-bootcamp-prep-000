398dc2f996bb3a5de2aa3b472fbf8a10cd4c95dc
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-11 19:09:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,13 @@
 def fizzbuzz(number)
+  a = number % 3
+  b = number % 5
+  if(a==0 && b==0)
+    return "FizzBuzz"
+  elseif(a==0)
+  return "Fizz"
+elseif (b==0)
+return "Buzz"
+else
+  return nil
+end 
+end

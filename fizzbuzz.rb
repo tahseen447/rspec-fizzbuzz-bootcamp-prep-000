1ddd59248f352a1d03b69608a9f222c794c89b26
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-11 19:24:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,12 @@ def fizzbuzz(number)
   #if(number % 3==0 && number % 5==0)
   #  "FizzBuzz"
   if (number%3==0 && number %5==0)
-    "FizzBuzz"(number % 3==0)
-  "Fizz"
+    "FizzBuzz"
+
 elsif (number % 5==0)
  "Buzz"
- elsif 
+ elsif(number % 3==0)
+     "Fizz"
 else
   nil
 end
